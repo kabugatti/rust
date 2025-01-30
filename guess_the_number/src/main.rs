be8b426042d3b42fe3c06f28cc44f94c2b84ca6a
Patrick Murimi <kabugap02@gmail.com>
2025-01-30 17:05:57 +0300

    chore: refactored guess_the_number

--- guess_the_number/src/main.rs
+++ guess_the_number/src/main.rs
@@ -2,15 +2,16 @@ use rand::Rng;
 use std::io;
 
 fn main() {
-    let random_num: i32 = gen_random_num();
+    // Get the random number
+    let random_number = gen_random_num();
 
-    println!("\nLet's play the number guessing game! 😋");
+    println!("\nLet's play 'Guess the number'! 🤗");
 
-    println!("\nGuess a number between 1 and 100: ");
+    println!("Guess a number between 1 and 100.");
 
+    
     loop {
         let mut rough_input = String::new();
-        println!("Number: ");
 
         io::stdin()
             .read_line(&mut rough_input)
@@ -19,22 +20,21 @@ fn main() {
         let input = rough_input.trim();
 
         if input.is_empty() {
-            println!("Please enter a number.");
+            println!("Please enter a number!");
             continue;
         }
 
         let parsed_input = match input.parse::<i32>() {
-            Ok(number) => number,
+            Ok(num) => num,
             Err(_) => {
-                println!("This is not a valid number!");
-                println!("Try again!");
+                println!("Enter a valid number.");
                 continue;
             }
         };
 
-        if parsed_input == -1 {
-            continue;
-        } else if check_if_correct(&parsed_input, &random_num) {
+        let is_correct = check_if_correct(&parsed_input, &random_number);
+
+        if is_correct {
             break;
         }
     }
@@ -45,15 +45,15 @@ fn gen_random_num() -> i32 {
     rng.random_range(1..=100)
 }
 
-fn check_if_correct(guess: &i32, correct_num: &i32) -> bool {
-    if guess == correct_num {
-        println!("Yaay! You guessed correctly! 🥳🥳🎇🎉😎");
-        true
-    } else if guess > correct_num {
+fn check_if_correct(guess: &i32, correct: &i32) -> bool {
+    if guess == correct {
+        println!("Yaay! You correctly guessed {}! 🎉🥳🎇😎💯✅", correct);
+        return true;
+    } else if guess > correct {
         println!("Go lower!");
-        false
     } else {
         println!("Go higher!");
-        false
     }
+
+    return false;
 }
