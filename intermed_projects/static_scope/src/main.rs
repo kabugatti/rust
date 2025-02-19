0b790a41b5e86da209a3146fc806fce0fd29413d
Patrick Murimi <kabugap02@gmail.com>
2025-02-20 00:02:09 +0300

    feat: learn about mutable global static variables

--- intermed_projects/static_scope/src/main.rs
+++ intermed_projects/static_scope/src/main.rs
@@ -2,12 +2,14 @@
 
 mod static_local;
 mod static_global;
+mod static_mutable;
 
 use static_global::{GLOBAL_MESSAGE, GLOBAL_TIMESTAMP};
 
 fn main() {
-    do_static_local_scope();
-    do_static_global_scope();
+    // do_static_local_scope();
+    // do_static_global_scope();
+    do_static_mutable_demo();
 }
 
 fn do_static_local_scope() {
@@ -17,4 +19,8 @@ fn do_static_local_scope() {
 
 fn do_static_global_scope() {
     println!("GLOBAL MESSAGE: {}, GLOBAL TIMESTAMP: {}", GLOBAL_MESSAGE, GLOBAL_TIMESTAMP.format("%T"));
+}
+
+fn do_static_mutable_demo() {
+    static_mutable::do_it();
 }
