b5e9371cc1635273b782de6d487c629dea42d151
Patrick Murimi <kabugap02@gmail.com>
2025-02-28 21:47:01 +0300

    chore: update static mutable code to compile

--- intermed_projects/static_scope/src/static_mutable.rs
+++ intermed_projects/static_scope/src/static_mutable.rs
@@ -6,26 +6,23 @@ pub fn do_it() {
 
     println!("\nIn static_mutable::do_it()");
 
-    unsafe {
-		f1();
-		f1();
-	}
+	f1();
+	f1();
 
 	f2();
 	f2();
 }
 
-unsafe fn f1() {
-	
-    static mut LOCAL_COUNT: i32 = 0;
+fn f1() {
+    static LOCAL_COUNT: AtomicI32 = AtomicI32::new(0);
 	let mut x = 0;
 
-	LOCAL_COUNT += 1;
 	x += 1;
+	LOCAL_COUNT.fetch_add(1, Ordering::Relaxed);
 	GLOBAL_COUNT.fetch_add(1, Ordering::Relaxed); 
 	
     println!("In f1, LOCAL_COUNT: {}, x: {}, GLOBAL_COUNT: {:?}", 
-              LOCAL_COUNT, x, GLOBAL_COUNT)
+              LOCAL_COUNT.load(Ordering::Relaxed), x, GLOBAL_COUNT)
 }
 
 fn f2() {
